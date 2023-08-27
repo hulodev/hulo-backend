c0638d38503c2b8824757e64e4f5ad7c71cab80d
Fareedat Bello <fareedatbello1@gmail.com>
2023-08-27 18:57:56 -0500

    [CONFIG] modify config file for jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,16 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  coverageReporters: ['lcov', 'text-summary']
+  coverageReporters: ['lcov', 'text-summary'],
+  collectCoverageFrom: ['src/**/*.ts'], //add files that we want to exclude from testing.
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80
+    }
+  }
 };
 
 export default config;
